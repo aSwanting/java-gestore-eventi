0b11d5bb50e48fef917e2b47fec734113216d146
aSwanting <gab.damico@gmail.com>
2024-04-09 10:47:55 +0200

    add - booking, cancelling, exception handling

--- src/org/learning/eventplanner/Event.java
+++ src/org/learning/eventplanner/Event.java
@@ -1,6 +1,9 @@
 package org.learning.eventplanner;
 
+import org.learning.eventplanner.exceptions.TimeTravelException;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Event {
     private final int totalCapacity;
@@ -8,26 +11,44 @@ public class Event {
     private LocalDate date;
     private int booked;
 
-    public Event(String title, LocalDate date, int totalCapacity) {
+    public Event(String title, LocalDate date, int totalCapacity) throws TimeTravelException {
+
         this.title = title;
-        this.date = date;
+        if (date.isBefore(LocalDate.now())) {
+            throw new TimeTravelException("Time Travel has not been invented yet...");
+        } else {
+            this.date = date;
+        }
         this.totalCapacity = totalCapacity;
+
     }
 
     // Class Methods
-    public void book() {
-
+    public void book() throws Exception {
+        if (booked == totalCapacity) {
+            throw new Exception("Event is at capacity");
+        } else if (date.isBefore(LocalDate.now())) {
+            throw new TimeTravelException("Cannot book past event");
+        } else {
+            booked++;
+        }
     }
 
-    public void cancel() {
-
+    public void cancel() throws Exception {
+        if (booked == 0) {
+            throw new Exception("Nothing left to cancel");
+        } else if (date.isBefore(LocalDate.now())) {
+            throw new TimeTravelException("Cannot cancel past event");
+        } else {
+            booked--;
+        }
     }
 
     @Override
     public String toString() {
         return "Event{" +
                 "title='" + title + '\'' +
-                ", date=" + date +
+                ", date=" + date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +
                 ", totalCapacity=" + totalCapacity +
                 ", booked=" + booked +
                 '}';
